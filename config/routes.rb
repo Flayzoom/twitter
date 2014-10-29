014284f87a54c0f00e6cb2d71cde0f164678b08f
Anton Kochegarov <Flayzoom@gmail.com>
2014-10-29 10:25:23 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root'static_pages#home'
+  
+  resources :users
+
+  
   match '/about', to: 'static_pages#about', via: :get
   match '/contact', to: 'static_pages#contact', via: :get
   match '/help', to: 'static_pages#help', via: :get
