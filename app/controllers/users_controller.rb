7099f472418aea37753b2dc55de15701119271e0
Anton Kochegarov <Flayzoom@gmail.com>
2014-10-31 04:20:32 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,31 @@
 class UsersController < ApplicationController
+ before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+    before_action :correct_user,   only: [:edit, :update]
   def new
       @user = User.new
   end
- 
-
+   def index
+    @users = User.paginate(page: params[:page])
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+   def index
+      @users = User.all
+   end
   def show
     @user = User.find(params[:id])
+     @microposts = @user.microposts.paginate(page: params[:page]
   end
    def create
     @user = User.new(user_params)
@@ -21,5 +41,23 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+        # Before filters
+
+    def signed_in_user
+        unless signed_in?
+        store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+        def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+        def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_ur
+    end
+      def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,10 @@ class User < ActiveRecord::Base
    def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
-
+ def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
+  end
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
@@ -33,4 +36,6 @@ class User < ActiveRecord::Base
       self.remember_token = User.encrypt(User.new_remember_token)
     end
   validates :password, length: { minimum: 6 }
+end
+    has_many :microposts, dependent: :destroy
 end
